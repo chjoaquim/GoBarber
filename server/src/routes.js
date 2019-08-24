52537af6919623e8beeb9e0cc74bf87e3d39b83b
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2019-08-24 09:50:31 -0300

    Incluindo cadastro de agendamento

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProvidersController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointment', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
