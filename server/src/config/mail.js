a281b49069cbe407e363ba3739652ecef167c3b4
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2019-08-25 10:41:01 -0300

    Usando mailtrap

---
+++ server/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e7ec4a0a337ab1',
+    pass: '8c3fac9455f7b2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
